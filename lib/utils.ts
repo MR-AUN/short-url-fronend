423b611878fdbb1893534e9185c5f93e483ee5b4
mr-aun <kiattisakjanjam@gmail.com>
2023-09-23 03:23:44 +0700

    edit url api

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 export const API_USER = axios.create({
-    baseURL: "http://localhost:8001/",
+    baseURL: "http://147.182.197.5:8001/",
     withCredentials: true
 })
 
 export const API_SHORTER = axios.create({
-    baseURL: "http://localhost:8002/",
+    baseURL: "http://147.182.197.5:8001/",
     withCredentials: true
 })
