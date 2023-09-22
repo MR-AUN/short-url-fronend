7a03c0d60a69c183dddd085b7e73d1a68334ffa2
mr-aun <kiattisakjanjam@gmail.com>
2023-09-23 04:37:56 +0700

    set config head2

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://147.182.197.5:8001 http://147.182.197.5:8002; object-src 'none';",
+            value: "default-src 'self' https:; connect-src 'self' https: http://147.182.197.5:8001;",
           },
         ],
       },
