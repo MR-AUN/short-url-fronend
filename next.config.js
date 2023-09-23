f84e7b4973c03607ef9db4f8528614a8b05b5afb
mr-aun <kiattisakjanjam@gmail.com>
2023-09-23 07:43:30 +0700

    default config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' https:; connect-src 'self' https: http://147.182.197.5:8001;",
-          },
-        ],
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
